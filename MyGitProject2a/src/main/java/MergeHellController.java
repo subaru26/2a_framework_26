45d03811069e0694461856d5f3a99aa666040fd4
S0621 <S0621@22S0621>
2023-11-14 15:13:01 +0900

    a

---
+++ MyGitProject2a/src/main/java/MergeHellController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+public class MergeHellController {
+	public static void main(String[] args) {
+		
+		kadaiC();
+	}
+	//void kadaiC(){
+	//ここに課題を書く。メソッド名変えてね。
+	
+	
+	int[a]=50
+			
+	int[b]=75
+	
+	int[C] = int[a] + int[b]
+	//}
+}
